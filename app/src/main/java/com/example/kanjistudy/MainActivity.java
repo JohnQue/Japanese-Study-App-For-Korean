31f9f989aeea0be09ab15b5db2e11bad046f710b
JohnQue <exuberant94@naver.com>
2019-09-06 16:31:59 +0900

    <--- Second Commit ---> 200 chinese words. I made a main activity and applied some design format.

--- app/src/main/java/com/example/kanjistudy/MainActivity.java
+++ app/src/main/java/com/example/kanjistudy/MainActivity.java
@@ -1,110 +1,34 @@
 package com.example.kanjistudy;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
-import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView baseView, meaningView;
-    final static int MY_PERMISSIONS_REQUEST_READ_FILE = 9876;
-    final static String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/TestLog/kanji.txt";
-    final static String jsonFilePath = "/storage/emulated/0/TestLog/kanji.json";
+
+    AppCompatButton randomStudy, unknown, onlyMeaning, onlyPronc, phraseStudy, verbStudy, termination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        baseView = findViewById(R.id.baseView);
-        meaningView = findViewById(R.id.meaningView);
-        fileRead();
-    }
+        randomStudy = findViewById(R.id.randomStudy);
+        unknown = findViewById(R.id.unknown);
+        onlyMeaning = findViewById(R.id.onlyMeaning);
+        onlyPronc = findViewById(R.id.onlyPronc);
+        phraseStudy = findViewById(R.id.phraseStudy);
+        verbStudy = findViewById(R.id.verbStudy);
+        termination = findViewById(R.id.termination);
 
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = getAssets().open("kanji.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return json;
-    }
-
-    public void fileRead() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                //보여줘야 할 경우에만 보여줌
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_FILE);
+        randomStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RandomStudyActivity.class);
+                startActivity(intent);
             }
-        } else {
-            JSONObject obj = null;
-            try {
-                obj = new JSONObject(loadJSONFromAsset());
-                String kanjiValue = obj.getString("hanza");
-                JSONArray kanjiArray = new JSONArray(kanjiValue);
-                JSONObject obj2 = kanjiArray.getJSONObject(0);
-                String kanji = obj2.getString("kanji");
-                String meaning = obj2.getString("meaning");
-                baseView.setText(kanji);
-                meaningView.setText(meaning);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_FILE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Log.d("Success", "You've got Permissions");
-                    JSONObject obj = null;
-                    try {
-                        obj = new JSONObject(loadJSONFromAsset());
-                        String kanjiValue = obj.getString("hanza");
-                        JSONObject kanjiObject = new JSONObject(kanjiValue);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Log.d("Denied", "Permission still denied");
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
+        });
     }
 }
